74a86b3f3c69105bf2004277397206512f3bd4fa
Nagraj544 <55784027+Nagraj544@users.noreply.github.com>
2021-12-09 12:46:26 +0530

    Add files via upload

---
+++ Exception2.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class Exception2 {
+	public static void main(String args[]) {
+		try {
+			System.out.println("Inside try block");
+			int data=25/0;
+			System.out.println(data);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Exception handled");
+			System.out.println(e);
+		}
+		finally {
+			System.out.println("finally block is always executed");
+			
+		}
+		System.out.println("rest of code");
+	}
+
+}
